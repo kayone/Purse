8459ae0117c64c286faed6aaf1b26fb876d90987
Keivan Beigi <kay.one@gmail.com>
2014-01-13 20:54:03 -0800

    switched to portable class library

--- src/Purse/Properties/AssemblyInfo.cs
+++ src/Purse/Properties/AssemblyInfo.cs
@@ -12,14 +12,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("42df3030-c88c-449a-9b9b-5f6f0b4e7ea7")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
