e391daae834322ee3f8dbc934971d7d80e7bb890
Keivan Beigi <kay.one@gmail.com>
2014-01-14 22:29:30 -0800

    marked CLSCompliant

--- src/Purse/Properties/AssemblyInfo.cs
+++ src/Purse/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -24,3 +25,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
