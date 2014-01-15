1f96531c4edce9acf12b777472f148313dc5abbf
Keivan Beigi <kay.one@gmail.com>
2014-01-14 22:29:39 -0800

    updated samples

--- src/Purse.Samples/ConfigSample.cs
+++ src/Purse.Samples/ConfigSample.cs
@@ -1,31 +1,49 @@
 ﻿using System;
+using System.Configuration;
 
 namespace Purse.Samples
 {
-    public class ThrottleSample
+public class ConfigSample
+{
+    private readonly Cache<string, string> _configCache;
+
+    public ConfigSample()
     {
-        public static void Main()
-        {
-            var _cache = new Cache<string, DateTime>();
-
-            while (true)
-            {
-                _cache.Get("Time", MethodThatShouldNotBeCalledVeryFrequently, TimeSpan.FromSeconds(2));
-            }
-
-        }
-
-        private static DateTime MethodThatShouldNotBeCalledVeryFrequently()
-        {
-            var time = DateTime.Now;
-            Console.WriteLine(time);
-            return time;
-        }
-
-        //OUTPUT
-        //2014-01-13 10:00:14 PM
-        //2014-01-13 10:00:16 PM
-        //2014-01-13 10:00:18 PM
-        //2014-01-13 10:00:20 PM
+        //Create a new instance of our cache that uses string for both key and value.
+        _configCache = new Cache<string, string>();
     }
+
+    public string Get(string key)
+    {
+        //Tries to get the request value from cache, if it doesn't exist
+        //call the function and retrieve the value. (this is generally called a read-through pattern)
+        return _configCache.Get(key, () => ReadValue(key));
+    }
+
+    public void Set(string key, string value)
+    {
+        //remove the existing value from cache
+        _configCache.Remove(key);
+
+        //write the new value to the backing store
+        WriteValue(key, value);
+
+        //update the cache with the new value
+        _configCache.Set(key, value);
+    }
+
+
+    private string ReadValue(string key)
+    {
+        //slow call to read config value from config store.
+        throw new NotImplementedException();
+    }
+
+    private void WriteValue(string key, string value)
+    {
+        //call to write config value to config store.
+        throw new NotImplementedException();
+    }
+
+}
 }
