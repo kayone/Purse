4f8ab4029c053c5d508b62953ed551a834d0e5ca
Keivan Beigi <me@keivan.io>
2014-10-29 11:21:37 -0700

    Added PrefetchedCache

---
+++ src/Purse.Tests/PrefetchedCacheFixture.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Purse.Tests
+{
+    [TestFixture]
+    public class PrefetchedCacheFixture
+    {
+        int _fetchCount = 0;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fetchCount = 0;
+        }
+
+        [Test]
+        public void should_populate_cache_on_creation()
+        {
+            var cache = new PrefetchedCache<string, int>(OnFetchFunc);
+            cache.Count.Should().Be(2);
+        }
+
+
+        [Test]
+        public void should_respect_long_expiry()
+        {
+            var cache = new PrefetchedCache<string, int>(OnFetchFunc, TimeSpan.FromHours(1));
+            cache.Get("a");
+            cache.Get("a");
+            cache.Get("a");
+
+            _fetchCount.Should().Be(1);
+        }
+
+        [Test]
+        public void should_respect_no_expiry()
+        {
+            var cache = new PrefetchedCache<string, int>(OnFetchFunc);
+            cache.Get("a");
+            cache.Get("a");
+            cache.Get("a");
+
+            _fetchCount.Should().Be(1);
+        }
+
+
+        [Test]
+        public void should_allow_manual_refresh_with_no_expiry()
+        {
+            var cache = new PrefetchedCache<string, int>(OnFetchFunc);
+            cache.Get("a");
+            cache.Get("a");
+            cache.Get("a");
+            _fetchCount.Should().Be(1);
+            cache.Refresh();
+            _fetchCount.Should().Be(2);
+        
+        }
+
+        [Test]
+        public void should_repopulate_cache_on_expiry()
+        {
+            var cache = new PrefetchedCache<string, int>(OnFetchFunc, TimeSpan.FromSeconds(1));
+            cache.Count.Should().Be(2);
+
+            Thread.Sleep(1000);
+
+            cache.Get("a");
+            cache.Get("a");
+            cache.Get("a");
+
+            _fetchCount.Should().Be(2);
+
+        }
+
+        private Dictionary<string, int> OnFetchFunc()
+        {
+            _fetchCount++;
+            return new Dictionary<string, int>
+            {
+                {"a", 1}, {"b", 2}
+            };
+        }
+    }
+}
---
+++ src/Purse/PrefetchedCache.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Purse
+{
+    /// <typeparam name="TKey">Type used for key. It's highly recommanded that you use value types eg. int, decimal and strings</typeparam>
+    /// <typeparam name="TValue">Type of value stored in cache.</typeparam>
+    /// <summary>Use this cache doesn't user lazy loading. All values are loaded and expired at the same time. use this for smaller set of items</summary>
+    public class PrefetchedCache<TKey, TValue>
+    {
+        private readonly Func<Dictionary<TKey, TValue>> _fetchFunc;
+        private readonly TimeSpan? _lifeTime;
+        private DateTime _expiry = DateTime.MaxValue;
+        private Dictionary<TKey, TValue> _items;
+
+        public PrefetchedCache(Func<Dictionary<TKey, TValue>> fetchFunc, TimeSpan? lifeTime = null)
+        {
+            _fetchFunc = fetchFunc;
+            _lifeTime = lifeTime;
+            Refresh();
+        }
+
+        /// <summary>
+        /// Return list of all values stored in cache
+        /// </summary>
+        public IEnumerable<TValue> Values
+        {
+            get
+            {
+                return _items.Values;
+            }
+        }
+
+        /// <summary>
+        /// Retrieve an item from cache
+        /// </summary>
+        /// <param name="key">Cache Key</param>
+        /// <returns>Cached value for the given key</returns>
+        public TValue Get(TKey key)
+        {
+            try
+            {
+                ValidateExpiry();
+                return _items[key];
+            }
+            catch (KeyNotFoundException)
+            {
+                throw new CacheKeyNotFoundException(key);
+            }
+        }
+
+        /// <summary>
+        /// Current number of items stored in cache
+        /// </summary>
+
+        public int Count
+        {
+            get
+            {
+                return _items.Count;
+            }
+        }
+
+        /// <summary>
+        /// Repopulate cache from source
+        /// </summary>
+        public void Refresh()
+        {
+            var items = _fetchFunc();
+            CalculateExpiry();
+            _items = items;
+        }
+
+        /// <summary>
+        /// Check if item exist in cache
+        /// </summary>
+        public bool ContainsKey(TKey key)
+        {
+            return _items.ContainsKey(key);
+        }
+
+        private void ValidateExpiry()
+        {
+            if (_expiry < DateTime.Now)
+            {
+                CalculateExpiry();
+                Refresh();
+            }
+        }
+
+        private void CalculateExpiry()
+        {
+            if (_lifeTime != null)
+            {
+                _expiry = DateTime.Now.Add(_lifeTime.Value);
+            }
+        }
+    }
+
+}
